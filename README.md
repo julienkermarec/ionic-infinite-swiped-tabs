592fd2cf78d9420f01a207a78cd739849a983787
Julien KERMAREC <contact@julienkermarec.com>
2018-07-08 15:11:48 +0200

    v1.0

---
+++ README.md
@@ -0,0 +1,67 @@
+# react-native-bar-chart
+
+![Screenshot](https://raw.githubusercontent.com/julienkermarec/ionic-infinite-swiped-tabs/master/screenshots/header.png)
+
+## Tutoriel
+
+https://blog.julienkermarec.com/ionic-3-infinite-swiped-tabs/
+
+## Features
+Infinite Tabs / Segments
+Auto-centering active segment with animation
+iOS / Android UX compatible
+
+## Usage
+```html
+<!-- TOOLBAR + SEGMENTS -->
+<ion-toolbar mode="md" color="light">
+  <ion-segment  #segments  mode="md" [(ngModel)]="page" color="light">
+      <ion-segment-button value="0"  (click)="selectedTab(0)">
+          Segment 1
+      </ion-segment-button>
+      <ion-segment-button value="1"  (click)="selectedTab(1)">
+          Segment 2
+      </ion-segment-button>
+      <ion-segment-button value="2"  (click)="selectedTab(2)">
+          Segment 3
+      </ion-segment-button>
+      <ion-segment-button value="3"  (click)="selectedTab(3)">
+          Segment 4
+      </ion-segment-button>
+      <ion-segment-button value="4"  (click)="selectedTab(4)">
+          Segment 5
+      </ion-segment-button>
+  </ion-segment>
+</ion-toolbar>
+<!-- SLIDES -->
+<ion-slides #slider (ionSlideDidChange)="slideChanged()">
+    <ion-slide>
+        <h1>Tab 1</h1>
+    </ion-slide>
+    <ion-slide>
+        <h1>Tab 2</h1>
+    </ion-slide>
+    <ion-slide>
+        <h1>Tab 3</h1>
+    </ion-slide>
+    <ion-slide>
+        <h1>Tab 4</h1>
+    </ion-slide>
+    <ion-slide>
+        <h1>Tab 5</h1>
+    </ion-slide>
+</ion-slides>
+
+```
+
+## TODO
+- [X] Ion-segment
+- [X] Ion-slides
+- [X] Center active segment
+- [X] Animate active segment
+
+## Info/Support
+
+If you need support, or business inquiry contact-me :
+
+@JulienKermarec - contact@julienkermarec.com
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,79 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component,ViewChild } from '@angular/core';
+import { NavController,Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('slider') slider: Slides;
+  @ViewChild("segments") segments;
+  page: any;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  // Initialize slider
+  ionViewDidEnter(){
+    this.slideChanged();
+  }
+
+  // On segment click
+  selectedTab(index) {
+    this.slider.slideTo(index);
+  }
+
+
+  // On slide changed
+  slideChanged() {
+    let currentIndex = this.slider.getActiveIndex();
+    this.page = currentIndex.toString();
+    this.centerScroll();
+  }
+
+  // Center current scroll
+  centerScroll(){
+    if(!this.segments || !this.segments.nativeElement)
+      return;
+
+    let sizeLeft = this.sizeLeft();
+    let sizeCurrent = this.segments.nativeElement.children[this.page].clientWidth;
+    let result = sizeLeft - (window.innerWidth / 2) + (sizeCurrent/2) ;
+
+    result = (result > 0) ? result : 0;
+    this.smoothScrollTo(result);
+  }
+
+  // Get size start to current
+  sizeLeft(){
+    let size = 0;
+    for(let i = 0; i < this.page; i++){
+      size+= this.segments.nativeElement.children[i].clientWidth;
+    }
+    return size;
+  }
+
+  // Easing function
+  easeInOutQuart(time, from, distance, duration) {
+    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+  }
+
+  // Animate scroll
+  smoothScrollTo(endX){
+    let startTime = new Date().getTime();
+    let startX = this.segments.nativeElement.scrollLeft;
+    let distanceX = endX - startX;
+    let duration = 400;
+
+    let timer = setInterval(() => {
+      var time = new Date().getTime() - startTime;
+      var newX = this.easeInOutQuart(time, startX, distanceX, duration),
+      if (time >= duration) {
+        clearInterval(timer);
+      }
+      this.segments.nativeElement.scrollLeft = newX;
+    }, 1000 / 60); // 60 fps
+  }
 }
